96f0a49fa3f09cdc21448e3fa1e7ceda4e41b16c
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-01 14:47:13 +0200

    added colon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {React} from 'react';
 import AppRoutes from './AppRoutes';
 import { Provider } from 'react-redux'; 
-import store from './redux/store'
+import store from './redux/store';
 
 function App() {
   return (
