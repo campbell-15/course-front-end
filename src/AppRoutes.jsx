48bf2030436ac79e207a41c771151b0fa306af6c
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-01 12:34:04 +0200

    added routes path

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StudentDetails from "./components/StudentDetails";
+import ManualCounter from "./components/ManualCounter";
+import CountUp from "./components/CountUp";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route  path="/" element={<StudentDetails />} />
+                <Route  path="/manual-counter" element={<ManualCounter />} />
+                <Route  path="/count-up" element={<CountUp />} />
 
             </Routes>
         </BrowserRouter>
